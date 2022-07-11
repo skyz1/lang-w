4cd733de8be2bc4ecc90690bdfe9564e9d804ed6
skyzl <glennskyzl@gmail.com>
2022-07-11 16:13:25 +0200

    Add tokenizer

---
+++ src/lang-w/tokenize.ts
@@ -0,0 +1,74 @@
+type TokenType = {
+    type: string,
+    regex: RegExp,
+    ignore?: boolean
+}
+
+const tokenTypes: Array<TokenType> = [
+    {
+        type: "keyword",
+        regex: /;|:=|skip|while|do|end|if|then|else/
+    },
+    {
+        type: "operator",
+        regex: /<=|>=|<>|\+|-|\||\*|\/|&|!|<|>|=/
+    },
+    {
+        type: "bool",
+        regex: /true|false/
+    },
+    {
+        type: "int",
+        regex: /[0-9]+/
+    },
+    {
+        type: "identifier",
+        regex: /[a-zA-Z_]+/
+    },
+    {
+        type: "whitespace",
+        regex: /\s+/,
+        ignore: true
+    }
+]
+
+export type Token = {
+    text: string,
+    type?: string,
+    index: number
+}
+
+export const tokenize = (code: string): Array<Token> => {
+    const tokens: Array<Token> = [];
+    var i = 0;
+
+    while (i < code.length) {
+        const remainingCode = code.substring(i)
+
+        const matched = tokenTypes.some(tokenType => {
+            const match = tokenType.regex.exec(remainingCode)
+            if (match && match.index === 0 && match[0].length > 0) {
+                if (!tokenType.ignore) {
+                    tokens.push({
+                        text: match[0],
+                        type: tokenType.type,
+                        index: i
+                    });
+                }
+                i += match[0].length
+                return true;
+            }
+            return false;
+        });
+
+        if (!matched) {
+            tokens.push({
+                text: code[i],
+                index: i
+            });
+            i += 1
+        }
+    }
+
+    return tokens;
+}

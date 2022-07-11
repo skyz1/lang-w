3aad318b65dcffe2ef022e1d010d4d073257d64a
skyzl <glennskyzl@gmail.com>
2022-07-11 16:13:41 +0200

    support tokenizer in the ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Token, tokenize } from './lang-w/tokenize';
 import logo from './logo.svg';
 
 function App() {
+  const [code, setCode] = useState<string>("")
+  const [tokens, setTokens] = useState<Array<Token>>([])
+
+  useEffect(() => {
+    setTokens(tokenize(code))
+  }, [code])
+
   return (
     <div className='flex flex-col m-4 space-y-2'>
       <h1 className='text-3xl text-bold'>LangW</h1>
       <p>This websites provides some tools to work with the simple programming language W and to visualize basic compiler concepts.</p>
       <p className='mt-4'>Your code:</p>
-      <textarea className='border-2 h-36'></textarea>
+      <textarea className='border-2 h-36' onChange={e => setCode(e.target.value)}></textarea>
       <p className='mt-4'>Compiler output:</p>
-      <div className='border-2 p-2 overflow-y-scroll h-36'>No output</div>
+      <div className='border-2 p-2 overflow-y-scroll h-36'>{
+        tokens.map((token, i) => (<p key={"token-" + i}>{token.type}({token.index}): {token.text}</p>))
+      }</div>
     </div>
   );
 }

cf9e78187fd817e87acf55890ca2495222c710d5
skyzl <glennskyzl@gmail.com>
2022-07-15 18:24:59 +0200

    Improve CollapsibleComponent Don't collapse when content is clicked

--- src/components/CollapsibleComponent.tsx
+++ src/components/CollapsibleComponent.tsx
@@ -6,11 +6,11 @@ function CollapsibleComponent(props: PropsWithChildren<{name: string}>) {
   return (
     <div className='border-2 bg-gray-100 p-2' onClick={() => { setCollapsed(c => !c) }}>
         <div className='flex flex-row'>
-            <div className='flex-auto font-bold select-none'>{props.name}:</div>
+            <div className='flex-auto font-bold select-none'>{props.name}</div>
             <div className='flex-none font-bold select-none'>{collapsed ? "+" : "-"}</div>
         </div>
         {!collapsed && 
-        <div className='border-2 bg-white mt-2 p-2 h-fit max-h-36 overflow-auto flex flex-col space-y-1'>
+        <div className='border-2 bg-white mt-2 p-2 h-fit max-h-36 overflow-auto flex flex-col space-y-1' onClick={e => e.stopPropagation()}>
             {props.children}
         </div>}
     </div>

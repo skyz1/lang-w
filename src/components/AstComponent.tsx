ea8017eb3262d42974d446e01333979001f75ac1
skyzl <glennskyzl@gmail.com>
2022-07-15 18:51:03 +0200

    Display ast nodes with correct height

--- src/components/AstComponent.tsx
+++ src/components/AstComponent.tsx
@@ -20,9 +20,9 @@ const AstComponent = ({root}: {root: AstNode} ) => {
     }
   
     if (rootChildren.length === 0) {
-      return <span className={'flex-auto mt-2 p-1 border border-black bg-blue-400 text-center'}>{text}</span>
+      return <span className={'flex-auto mt-2 p-1 border border-black bg-blue-400 text-center h-fit'}>{text}</span>
     } else {
-      return <span className={'flex-none w-fit'}>
+      return <span className={'flex-none w-fit h-fit'}>
         <div className={'mt-2 p-1 border border-black bg-blue-400 w-full text-center'} onClick={() => setCollapsed(c => !c)}>{text}</div>
         {collapsed || <span className='flex flex-nowrap w-full space-x-2'>
           {rootChildren.map((child, i) => <AstComponent root={child} key={"child-" + i}></AstComponent>)}

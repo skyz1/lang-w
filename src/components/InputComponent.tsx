610a52b438ae96d2470fcdf023b1969e409a0b4a
skyzl <glennskyzl@gmail.com>
2022-07-16 20:27:13 +0200

    Improve code input

--- src/components/InputComponent.tsx
+++ src/components/InputComponent.tsx
@@ -1,7 +1,17 @@
 function InputComponent(props: { code: string, pipeline: string, inputChanged: (code: string, pipeline: string) => void }) {
     return <>
         <p className='mt-4 font-bold'>Your code:</p>
-        <textarea className='border-2 h-36' onChange={e => props.inputChanged(e.target.value, props.pipeline)} value={props.code}></textarea>
+        <div className='border-2 h-fit relative overflow-y-auto'>
+            <div className='h-fit w-full font-mono resize-none z-10 relative'>
+                {props.code.split("\n").map((line, i, arr) => 
+                    <div key={i} className={"flex flex-row " + (i < arr.length-1 ? "leading-[30px] border-b-2" : "leading-[32px]")}>
+                        <span className='pl-2 w-8 font-bold'>{i}</span>
+                        <span className='pl-1 border-l'>{line}</span>
+                    </div>
+                )}
+            </div>
+            <textarea className='text-transparent h-full w-full caret-black font-mono resize-none absolute bg-transparent pl-9 leading-[32px] absolute top-0 outline-0 z-50' onChange={e => props.inputChanged(e.target.value, props.pipeline)} value={props.code}></textarea>
+        </div>
         <div className='flex flex-row'>
             <span className='flex-none text-bold mr-4'>Pipeline: </span>
             <select className='flex-auto border-2' value={props.pipeline} onChange={e => props.inputChanged(props.code, e.target.value)}>
